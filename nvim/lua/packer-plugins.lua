e429c4093ca68ae4bf22827ad64948d245f6a83f
Tommaso Lauria <tommasolauria98@gmail.com>
2023-07-23 13:28:58 +0200

    feat: nvim

---
+++ nvim/lua/packer-plugins.lua
@@ -0,0 +1,8 @@
+return require('packer').startup(function()
+	--packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	--colorscheme
+	use 'EdenEast/nightfox.nvim'
+	use 'EdenEast/duskfox.nvim'
+end)
